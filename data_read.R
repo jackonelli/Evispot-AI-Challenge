f964c3c95d75403b66ad80e895630f6a97ef93eb
Jakob Lindqvist <jakob.lindqvist.92@gmail.com>
2017-05-12 13:18:13 +0200

    Data clean v1.1

--- data_read.R
+++ data_read.R
@@ -7,6 +7,8 @@ library(klaR)
 library(caret)
 library(plyr)
 library(MASS)
+#WEEKDAYS
+library(lubridate)
 
 # READ ORIGINAL DATA
 train <- read.csv2("training_data.csv", sep = ",", header = T)
@@ -14,9 +16,11 @@ train <- read.csv2("training_data.csv", sep = ",", header = T)
 N <- dim(train)[1]
 p <- dim(train)[2]
 
+# TURN SHITTY DATE FACTOR INTO YEAR; MONTH; DAY; WEEKDAY
 day_vec <- rep(0,N)
 month_vec<- day_vec
 year_vec <- day_vec
+wday_vec <- day_vec
 
 for (i in 1:N){
   datetxt <- as.character(train$DATE[i])
@@ -24,15 +28,24 @@ for (i in 1:N){
   month_vec[i] <- as.numeric(datetxt[[1]][[1]])
   day_vec[i] <- as.numeric(datetxt[[1]][[2]])
   year_vec[i] <- as.numeric(datetxt[[1]][[3]])
+  wday_vec[i] <- wday(sprintf("%d-%d-%d", year_vec[i],month_vec[i],day_vec[i]))
+  
 }
 train$DAY <- day_vec
 train$MONTH <- month_vec
 train$YEAR <- year_vec
+train$WDAY <- wday_vec
 
+#REMOVE SHITTY DATE; COUNTRY (~98% SE) AND ONE TOT CORR FACTOR
 train <- train[,-c(2, 5, 9)]
-train <- train[,c(1:6,8,9,10,7)]
+# PERMUTE TO MOVE CLASS LAST 
+train <- train[,c(1:6,8,9,10,11,7)]
 
-write.table(train, file = "train_date.csv", sep=" ")
+#REMOVE NA
+train <- train[!is.na(train$BIRTH_YEAR),]
+
+write.table(train, file = "train_clean.csv", sep=" ")
+
 
 counts <- count(train$KEYWORD)
 rel_counts <-counts$freq / N
