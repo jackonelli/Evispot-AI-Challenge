ea716f912e5596e07a88ea81a00a1b2e23f8f12d
Per Nilsson Lundberg <pernlundberg@gmail.com>
2017-05-15 20:55:04 +0200

    Update sheiess

--- rcode.R
+++ rcode.R
@@ -2,7 +2,7 @@ library(MASS)
 library(caret)
 library(ranger)
 library(glmnet)
-setwd('~/dev/evispot')
+setwd('~/dev/evispot/Evispot-AI-Challenge')
 #train <-read.table("training_data.csv",sep=",",header=TRUE)
 #nearZeroVar(train[,-11])
 #x11()
@@ -12,16 +12,14 @@ setwd('~/dev/evispot')
 #}
 
 #nSample = dim(train)[1]
-nSample = 40000
+nSample = 50000
 sTrain <-sample(seq(1,dim(train)[1]),nSample)
 subTrain = train[sTrain,]
-subTrain = droplevels(subTrain)
-#magic_vec = as.numeric(subTrain$TRANS_AMO%%1==0)
-#magic_vec = as.numeric(subTrain$TRANS_AMO%%1==0)+1
-magic_vec = as.factor(as.numeric(subTrain$TRANS_AMO%%1==0)+1)
-subTrain$DEC = magic_vec
-subTrain = subTrain[c(1:10,12,11)]
-subTrain <- subTrain[,c(-2,-6)]
+subTrainKey = subTrain[,2] 
+subTrain = subTrain[c(-2,-3,-7,-8,-9)]
+#subTrain[,2] <- as.factor(subTrain[,2])
+#subTrain <- subTrain[,c(1,5,7,8,9,10)]
+#subTrain <- subTrain[,c(-2,-6)]
 #subTrain <- train[sTrain,c(-1,-2,-3,-4,-6)]
 #subTrain <- train[sTrain,-6]
 #subTrain <- train[sTrain,c(2,5,11)]
@@ -32,6 +30,7 @@ trainSample <- sample(nSample,trainQuota*nSample)
 
 subTrain.train = subTrain[trainSample,]
 subTrain.test = subTrain[-trainSample,]
+subTrain.testkey = subTrainKey[-trainSample]
 
 magic_label <- dim(subTrain)[2]
 x.train = subTrain.train[,-magic_label]
@@ -67,14 +66,14 @@ fitControl <- trainControl(
   number = 3,
   classProbs = TRUE)
 #
-#ccnb<-train(x=x.train,y=y.train,method="nb",trControl = fitControl)
-#pp<-predict(ccnb,x.test,type="prob")
-#pp$obs<-y.test
-#pp$pred<-predict(ccnb,x.test)
-#AccN<-multiClassSummary(pp,lev=levels(pp$obs))
-#print(AccN)
+# ccnb<-train(x=x.train,y=y.train,method="nb",trControl = fitControl)
+# pp<-predict(ccnb,x.test,type="prob")
+# pp$obs<-y.test
+# pp$pred<-predict(ccnb,x.test)
+# AccN<-multiClassSummary(pp,lev=levels(pp$obs))
+# print(AccN)
  
-ccrf<-train(x=x.train,y=y.train,method="ranger",trControl = fitControl)
+ccrf<-train(x=x.train,y=y.train,method="ranger",trControl = fitControl,num.trees=100)
 pp<-predict(ccrf,x.test,type="prob")
 ppYOLO<-predict(ccrf,x.test,type="prob")
 pp$obs<-y.test
@@ -107,12 +106,12 @@ print(AccR)
 #  print(sum(cool_matrix[,i]==true_dec) / dim(train)[1])
 #}
 
-true_dec = as.numeric(train$BIRTH_YEAR<=1930)
-cool_matrix = matrix(nrow = dim(train)[1],ncol=14)
-for(i in 1:14){
-  cool_matrix[,i]= as.numeric(train$KEYWORD==levels(train$KEYWORD)[i])
-  print(sum(cool_matrix[,i]==true_dec) / dim(train)[1])
-}
+#true_dec = as.numeric(train$BIRTH_YEAR<=1930)
+#cool_matrix = matrix(nrow = dim(train)[1],ncol=14)
+#for(i in 1:14){
+# cool_matrix[,i]= as.numeric(train$KEYWORD==levels(train$KEYWORD)[i])
+# print(sum(cool_matrix[,i]==true_dec) / dim(train)[1])
+#}
 
 
 # USR INDEX
@@ -134,6 +133,8 @@ for (i in 1:N){
   }
 }
 usr_idx[j,2] <- N
+usr_idx[j,3] <- train$Key_ENGNO[N]
+
 
 # KEYWORD DIST PER USER
 
@@ -143,6 +144,23 @@ for (i in 1:n_use){
 }
 obs_user <- rep(0,ceiling((1-trainQuota)*nSample))
 for (i in 1:ceiling((1-trainQuota)*nSample)){
-obs_user[i] <-which(usr_idx[,3] == train$Key_ENGNO[i])
+  if(i!=1835)
+obs_user[i] <-which(usr_idx[,3] == subTrain.testkey[i])
 }
-key_dist[obs_user, ]
+
+bestRes = 0
+tmp = 0
+ind = 0
+for(i in seq(0.01,1,0.01)){
+result <- (i*key_dist[obs_user,]+(1-i)*ppYOLO)
+maxRes <- apply(result,1,which.max)
+tmp = sum(maxRes==as.numeric(y.test)) / length(y.test)
+if(tmp > bestRes){
+  bestRes = tmp
+  ind = i
+  print(bestRes)
+}
+}
+
+#sheissVec = cbind(key_dist[obs_user,],as.matrix(ppYOLO))
+#ccnbASDASD<-train(x=sheissVec,y=y.test,method="ranger",trControl = fitControl)
