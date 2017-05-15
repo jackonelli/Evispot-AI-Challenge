3712f8f32245b772c46f2a196693839354498343
Per Nilsson Lundberg <pernlundberg@gmail.com>
2017-05-15 19:24:17 +0200

    Updated dataread

--- data_read.R
+++ data_read.R
@@ -1,4 +1,4 @@
-setwd("~/EZW")
+setwd('~/dev/evispot/Evispot-AI-Challenge')
 library(randomForest)
 library(xtable)
 library(plot3D)
@@ -44,36 +44,7 @@ train <- train[,c(1:6,8,9,10,11,7)]
 #REMOVE NA
 train <- train[!is.na(train$BIRTH_YEAR),]
 
-write.table(train, file = "train_clean.csv", sep=" ")
-
-
-counts <- count(train$KEYWORD)
-rel_counts <-counts$freq / N
-par(las=2)
-plot(counts$x, rel_counts)
-
-na_train <- train[!is.na(train$BIRTH_YEAR),]
-na_counts <- count(na_train$KEYWORD)
-na_rel_counts <-na_counts$freq / N
-par(las=2)
-plot(na_counts$x, na_rel_counts)
-
-
-# RELATIVE COUNTS
-counts$freq[counts$x == "grocery_store"] / sum(counts$freq)
-
-names(train)
-
-library(corrplot)
-cor(t(as.factor(train)))
-train_f <- as.factor(train)
-
-n <- 2
-samp_ind <- sample(x = seq(1 : N) , size = n, replace = F)
-sub_train <- as.numeric(na_train[samp_ind,])
-
-lda( sub_train[,10] ~ ., data = sub_train)
-
-
-  fit <- train(KEYWORD ~ ., data=trainSet[trainInd, ], method="knn",tuneLength=15,trControl=ctrl)
-  pp <- predict(fit, newdata=gener[-trainInd, -4], type="raw")
+magic_vec = as.factor(as.numeric(train$TRANS_AMO%%1==0)+1)
+train$DEC = magic_vec
+train = droplevels(train)
+train = train[c(1:10,12,11)]
